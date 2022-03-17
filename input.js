1c25782893854ef395053f86bee7b3ef5e9d2324
robotsch <christianhumble22@gmail.com>
2022-03-16 20:47:09 -0400

    initial commit

---
+++ input.js
@@ -0,0 +1,14 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.on("data", handleUserInput);
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if(key === '\u0003') process.exit()
+};
+
+module.exports = setupInput
