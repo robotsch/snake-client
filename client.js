1537617e99ce7eee687f16f4192f8a7003fdd0e6
robotsch <christianhumble22@gmail.com>
2022-03-16 21:06:26 -0400

    lint code

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const net = require("net");
 
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: 50541
@@ -10,15 +10,15 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on('connect', () => {
-    conn.write('Name: chr')
-  })
+    conn.write('Name: chr');
+  });
 
   conn.on('data', (data) => {
-    console.log(data)
+    console.log(data);
     
-  })
+  });
 
   return conn;
 };
 
-module.exports = connect
+module.exports = connect;
--- input.js
+++ input.js
@@ -1,6 +1,6 @@
-let connection
+let connection;
 
-const setupInput = function (conn) {
+const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
@@ -10,31 +10,31 @@ const setupInput = function (conn) {
   return stdin;
 };
 
-const handleUserInput = function (key) {
-  if(key === '\u0003')
+const handleUserInput = function(key) {
+  if (key === '\u0003') process.exit();
   switch (key) {
 
-    case 'w':
-      connection.write('Move: up');
-      break;
-    case 'a':
-      connection.write('Move: left');
-      break;
-    case 's':
-      connection.write('Move: down');
-      break;
-    case 'd':
-      connection.write('Move: right');
-      break;
-    case 'b':
-      connection.write('Say: Boop')
-      break
-    case 'f':
-      connection.write('Say: Paying respects')
-      break
-    }
+  case 'w':
+    connection.write('Move: up');
+    break;
+  case 'a':
+    connection.write('Move: left');
+    break;
+  case 's':
+    connection.write('Move: down');
+    break;
+  case 'd':
+    connection.write('Move: right');
+    break;
+  case 'b':
+    connection.write('Say: Boop');
+    break;
+  case 'f':
+    connection.write('Say: Paying respects');
+    break;
+  }
 };
 
 
 
-module.exports = setupInput
+module.exports = setupInput;
